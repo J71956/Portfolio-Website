ce9cc6b31c5bf9ed52f953afdc2209048c442695
Siu Yau Shing <90333201+J71956@users.noreply.github.com>
2025-01-03 00:56:08 +0800

    ahhh

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput 
+echo "BUILD START"
+
+# Install dependencies
+python3 -m pip install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --noinput
+
+# Create Vercel-compatible output vercel directory
 mkdir -p .vercel/output/static
 cp -r staticfiles/ .vercel/output/static/
 
+# Apply database migrations
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+echo "BUILD END"

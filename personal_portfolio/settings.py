e719687655fde70d572e7c746a14e1c8d3f29f90
Siu Yau Shing <90333201+J71956@users.noreply.github.com>
2025-01-02 23:58:40 +0800

    plswork

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.vercel.app']
 
 INSTALLED_APPS = [
     "pages.apps.PagesConfig",
+    ""
     "projects.apps.ProjectsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -120,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

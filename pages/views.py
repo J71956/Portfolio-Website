aa9d3125d346dedacf6869792a819d61af42ac86
Siu Yau Shing <90333201+J71956@users.noreply.github.com>
2025-01-02 22:13:44 +0800

    Initial commit

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+# pages/views.py
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "pages/home.html", {})
+
+def contact(request):
+    return render(request, "pages/contact.html", {})

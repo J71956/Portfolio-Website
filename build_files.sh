c8e3bbf230434747225a0de42fc454d9d20d8a61
Siu Yau Shing <90333201+J71956@users.noreply.github.com>
2025-01-03 01:03:47 +0800

    doing stuff

--- build_files.sh
+++ build_files.sh
@@ -2,8 +2,18 @@
 
 echo "BUILD START"
 
+# Add /python312/bin to PATH
+export PATH="/python312/bin:$PATH"
+
+# Create and activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
 # Install dependencies
 python3 -m pip install -r requirements.txt
+
+# Verify Django installation
+python3 -m django --version
 
 # Collect static files
 python3 manage.py collectstatic --noinput
